64fba53847de10366b8f75323331e6cc4a3720e5
Bobby-Ling <bobby-ling@outlook.com>
2024-07-27 21:52:12 +0800

    Genochemy Demo OK

--- Python/test.py
+++ Python/test.py
@@ -1,3 +1,27 @@
-for i in range(0,50):
-    M=i
-    print((M+1)//2==M//2+1)
+class A:
+    global a
+    def execute(self):
+        global a
+        a=2
+
+
+a=1
+aaa=A()
+aaa.execute()
+print(a)
+
+class B:
+    num: int = 0
+    def __hash__(self) -> int:
+        return self.num
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, B):
+            return self.num == value.num
+        return False
+
+pool = {}
+b = B()
+b.num = 111
+pool[b] = "abc"
+pool[None] = "cde"
+print(pool)
